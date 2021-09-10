05cf69975466eef58334b34a091fe20ecf4133e3
Tushar Kharat <tushar@Tushars-MacBook-Pro-M1.local>
2021-09-10 11:20:22 +0530

    fix: fixed error in calculateKw function

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,7 +1,9 @@
 import { Solar } from "network/api/solars/schemas";
 
 function calculateKW(data: Solar[]){
-    return data.reduce((accumulator, current) => accumulator + Number(current.wattage), 0)/1000;
+    if(data != undefined){
+        return data.reduce((accumulator, current) => accumulator + Number(current.wattage), 0)/1000;
+    }
 }
 
 export default calculateKW
